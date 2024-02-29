ae492f9c1dc5c8a3cd41c5dcbda9e48ecd4b582d
dineshbekkam11 <dineshbekkam@gmail.com>
2024-02-29 10:00:28 -0600

    Check commit

---
+++ src/main/java/name.java
@@ -0,0 +1,16 @@
+public class name {
+    private final String name;
+
+    public name(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+
+    }
+
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+}
